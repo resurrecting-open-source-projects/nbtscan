ec33966582e7705f9f19749ede72ea27ff23f43d
Mayco <mayco.s.berghetti@gmail.com>
2021-12-16 14:38:51 -0400

    fixed comparison less than 0 in unsigned type

--- src/range.c
+++ src/range.c
@@ -74,8 +74,9 @@ is_range1 ( char *string, struct ip_range *range )
     {
       separator++;
       mask = atoi ( separator );
-      if ( mask <= 0 || mask > 32 )
+      if ( mask == 0 || mask > 32 )
         return 0;
+        
       strcpy ( ip, string );
       ip[abs ( ( int ) ( string - separator ) ) - 1] = 0;
       if ( ( range->start_ip = inet_addr ( ip ) ) == INADDR_NONE )
